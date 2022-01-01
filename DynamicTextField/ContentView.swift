1958bb0fd20fbf95d597230b27dcd888be303bd5
Taichi Uragami <tu.0514.0315@icloud.com>
2022-01-01 15:57:27 +0900

    Initial Commit

---
+++ DynamicTextField/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DynamicTextField
+//
+//  Created by Taichi Uragami on 2022/01/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DynamicTextField/DynamicTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicTextFieldApp.swift
+//  DynamicTextField
+//
+//  Created by Taichi Uragami on 2022/01/01.
+//
+
+import SwiftUI
+
+@main
+struct DynamicTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
