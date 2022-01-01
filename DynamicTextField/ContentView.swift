143cc6882cb12afe9954d5c70ce6b2aa48ad04fd
Taichi Uragami <tu.0514.0315@icloud.com>
2022-01-01 16:31:16 +0900

    Implement dynamic textfield

--- DynamicTextField/ContentView.swift
+++ DynamicTextField/ContentView.swift
@@ -8,9 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var wordNames: [String] = [""]
+    @State var translations: [String] = [""]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView {
+            VStack {
+                ForEach(0..<wordNames.count, id: \.self) { index in
+                    HStack {
+                        Text("単語名:")
+                        TextField("入力", text: self.$wordNames[index]).foregroundColor(.blue)
+                        Text("意味:")
+                        TextField("入力", text: self.$translations[index]).foregroundColor(.blue)
+                        if index == wordNames.count - 1 {
+                            Button(action:{
+                                self.wordNames.append("")
+                                self.translations.append("")
+                            }) {
+                                Text("Add more")
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
